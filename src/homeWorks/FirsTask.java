f78d1e3cb106f4eb9ee16c7b046fa9ac5fea03bb
DesislavaTor <destorbeva@gmail.com>
2025-07-27 12:16:49 +0200

    init commit & first task

---
+++ src/homeWorks/FirsTask.java
@@ -0,0 +1,11 @@
+package homeWorks;
+
+public class FirsTask {
+  public static void main(String[] args) {
+    String firstName = "Desislava";
+    String middleName = "Ivanova";
+    String lastName = "Torbeva";
+
+    System.out.println(firstName + " " + middleName + " " + lastName);
+  }
+}
