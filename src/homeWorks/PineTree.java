f711b4cb001e57d35ceef04b68e68b5f0672d83b
DesislavaTor <destorbeva@gmail.com>
2025-07-27 17:15:52 +0200

    pine tree task

---
+++ src/homeWorks/PineTree.java
@@ -0,0 +1,11 @@
+package homeWorks;
+
+public class PineTree {
+  public static void main(String[] args) {
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println("*****");
+
+    System.out.println("  *  ");
+  }
+}
