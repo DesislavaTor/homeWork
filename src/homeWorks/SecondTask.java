8aa0ca2f10d9fb339791868c466b7b42c9b530cf
DesislavaTor <destorbeva@gmail.com>
2025-07-27 16:56:55 +0200

    second task

---
+++ src/homeWorks/SecondTask.java
@@ -0,0 +1,12 @@
+package homeWorks;
+
+public class SecondTask {
+  public static void main(String[] args) {
+    int SideA = 5;
+    int SideB = 6;
+    int SideC = 7;
+    int perimeter = SideA + SideB + SideC;
+
+    System.out.println(perimeter);
+  }
+}
